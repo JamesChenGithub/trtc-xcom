239796aed1336aa4d538f7771202a2537065e657
AlexiChen <>
2019-03-07 14:44:05 +0800

    update annotation

--- xcom/log/PtrBuf/xcom_log_delc.h
+++ xcom/log/PtrBuf/xcom_log_delc.h
@@ -30,7 +30,7 @@ extern "C" {
     } xcom_log_level;
     
     typedef struct xcom_log_item_t {
-        xcom_log_level level;
+        xcom_log_level level;       
         const char *tag;
         const char *file_name;
         const char *func_name;
@@ -42,41 +42,43 @@ extern "C" {
         intmax_t maintid;
     } xcom_log_item;
     
+    
+    // get the cur process id
     extern intmax_t xcom_log_get_pid(void);
-    
+    // get the cur thread id
     extern intmax_t xcom_log_get_tid(void);
-    
+    // get the cur main thread id
     extern intmax_t xcom_log_get_main_tid(void);
-    
+
+    // delc the append log func
     typedef void (*xcom_log_append_log_func)(const xcom_log_item *info, const char *log);
-    
+
     extern const char *xcom_log_dump(const void *dumpbuffer, size_t len);
     
+    // get the log level
     xcom_log_level xcom_log_get_log_level(void);
-    
+    // set the log level
     void xcom_log_set_log_level(xcom_log_level level);
-    
+    // is enable for log level
     int xcom_log_is_enabled_for_level(xcom_log_level level);
-    
+    // send the append log func
     xcom_log_append_log_func xcom_log_set_append_log_func(xcom_log_append_log_func appender);
     
-    // no level filter
+    // xcom assert log
 #ifdef __GNUC__
-    
     __attribute__((__format__(printf, 3, 4)))
 #endif
     void xcom_log_assert_p(const xcom_log_item *info, const char *expression, const char *format, ...);
     
     void xcom_log_assert(const xcom_log_item *info, const char *expression, const char *log);
     
+    // write log to cache
 #ifdef __GNUC__
-    
     __attribute__((__format__(printf, 2, 0)))
 #endif
     void xcom_log_vprint(const xcom_log_item *info, const char *format, va_list list);
     
 #ifdef __GNUC__
-    
     __attribute__((__format__(printf, 2, 3)))
 #endif
     void xcom_log_print(const xcom_log_item *info, const char *format, ...);
